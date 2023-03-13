e25208ea490983a544aac33429a43c43153764e9
root <root@pim.jhiguera.es>
2023-03-13 20:00:08 +0000

    Initial commit

---
+++ Setup/Patch/Data/AddCustomAttributeSet.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Vendor\ModuleName\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Eav\Model\Entity\Attribute\SetFactory as AttributeSetFactory;
+use Magento\Catalog\Setup\CategorySetupFactory;
+
+class AddCustomAttributeSet implements DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var AttributeSetFactory
+     */
+    private $attributeSetFactory;
+
+    /**
+     * @var CategorySetupFactory
+     */
+    private $categorySetupFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param AttributeSetFactory $attributeSetFactory
+     * @param CategorySetupFactory $categorySetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        AttributeSetFactory $attributeSetFactory,
+        CategorySetupFactory $categorySetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->attributeSetFactory = $attributeSetFactory;
+        $this->categorySetupFactory = $categorySetupFactory;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function apply()
+    {
+        $setup = $this->moduleDataSetup;
+        $setup->startSetup();
+        $categorySetup = $this->categorySetupFactory->create(['setup' => $setup]);
+        $attributeSet = $this->attributeSetFactory->create();
+        $entityTypeId = $categorySetup->getEntityTypeId(\Magento\Catalog\Model\Product::ENTITY);
+        $attributeSetId = $categorySetup->getDefaultAttributeSetId($entityTypeId); // Default attribute set Id
+        $data = [
+            'attribute_set_name' => 'Custom_Product_attribute_set', // custom attribute set name
+            'entity_type_id' => $entityTypeId,
+            'sort_order' => 50,
+        ];
+        $attributeSet->setData($data);
+        $attributeSet->validate();
+        $attributeSet->save();
+        $attributeSet->initFromSkeleton($attributeSetId)->save(); // based on default attribute set
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
