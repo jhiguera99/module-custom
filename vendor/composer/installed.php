16b5967724c9b92ab1886a4d8c8ea5063adc0d12
root <root@pim.jhiguera.es>
2023-03-13 20:55:41 +0000

    second release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jhiguera99/module-custom',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '26b7c80911b5a33cd54243b9ed57c5650a94349d',
+        'type' => 'magento2-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'custom-module-jh/module-custom' => array(
+            'pretty_version' => '0.0.1-beta',
+            'version' => '0.0.1.0-beta',
+            'reference' => '228e4e1fd7c1d00cb2536d5d5f72c221ad828cb0',
+            'type' => 'magento2-module',
+            'install_path' => __DIR__ . '/../custom-module-jh/module-custom',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'jhiguera99/module-custom' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '26b7c80911b5a33cd54243b9ed57c5650a94349d',
+            'type' => 'magento2-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
